f7a0125f2a3e5dbbc0d860edbbafbf27e15bcb53
Nami Sunami <naoyuki.sunami@gmail.com>
2021-10-07 16:35:24 -0400

    render review per user

--- app/core/components/ReviewList.tsx
+++ app/core/components/ReviewList.tsx
@@ -1,5 +1,36 @@
 import React from "react"
+import { useQuery } from "@blitzjs/core"
+import { Review } from "./Review"
+import { useCurrentUser } from "../hooks/useCurrentUser"
+import getReviewAnswers from "app/queries/getReviewAnswers"
 
-export const ReviewList = () => {
-  return <div>This is a review list</div>
+export const ReviewList = (prop) => {
+  const { article } = prop
+  const currentUser = useCurrentUser()
+
+  const [reviews] = useQuery(getReviewAnswers, {
+    currentArticleId: article?.id,
+  })
+
+  const isWrittenBy = (answer, userId) => {
+    return answer.userId === userId
+  }
+
+  const userIds = Array.from(new Set(reviews.map((review) => review.userId)))
+
+  userIds.forEach((userId) => {
+    const reviewsPerUser = reviews.filter(({ userId }) => userId === 1)
+  })
+
+  return (
+    <>
+      <div>Reviews</div>
+      {userIds.map((userId) => {
+        const reviewsPerUser = reviews.filter((review) => review.userId === userId)
+        return <Review key={userId} userId={userId} review={reviewsPerUser} />
+      })}
+
+      {/* {JSON.stringify(reviews)} */}
+    </>
+  )
 }
