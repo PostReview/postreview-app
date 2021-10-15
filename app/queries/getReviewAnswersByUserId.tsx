1b2a7f48c42eb4253773c87ebf4f0c03b48168fd
Nami Sunami <naoyuki.sunami@gmail.com>
2021-10-15 17:03:51 -0400

    add a query to get review answers w/ article info

---
+++ app/queries/getReviewAnswersByUserId.tsx
@@ -0,0 +1,11 @@
+import db from "db"
+
+export default async function getReviewAnswers(props) {
+  const { currentUserId } = props
+  return await db.reviewAnswers.findMany({
+    where: { userId: currentUserId },
+    include: {
+      targetArticle: true,
+    },
+  })
+}
