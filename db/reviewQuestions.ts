9f0b5870eccbadc57935148f556cc54185439043
Nami Sunami <naoyuki.sunami@gmail.com>
2021-10-27 06:42:37 -0400

    update review questions

--- db/reviewQuestions.ts
+++ db/reviewQuestions.ts
@@ -2,37 +2,35 @@ export const reviewQuestions = [
   {
     questionId: 1,
     questionCategory: "Research Question",
-    questionText: "Do you find the research question meaningful and worthwhile?",
+    questionText: "Do you find the research question worthwhile?",
     minLabel: "Not at all",
     maxLabel: "Completely",
   },
   {
     questionId: 2,
     questionCategory: "Design",
-    questionText:
-      "Do you approve the way the study has been conducted? Is it suited to answer the research questions? ",
+    questionText: "Do you find the way the study was conducted adequate?",
     minLabel: "Not at all",
     maxLabel: "Completely",
   },
   {
     questionId: 3,
     questionCategory: "Findings",
-    questionText:
-      "Do you find the pattern of the results or the narrative sufficient to support the claim?",
+    questionText: "Do you consider the findings sufficient to support the authors' claims?",
     minLabel: "Not at all",
     maxLabel: "Completely",
   },
   {
     questionId: 4,
     questionCategory: "Interpretation",
-    questionText: "Do you find the explanations authors propose plausible and believable?",
+    questionText: "Do you find the authors' explanation plausible?",
     minLabel: "Not at all",
     maxLabel: "Completely",
   },
   {
     questionId: 5,
     questionCategory: "Significance",
-    questionText: "How beneficial do you find this study for humanity as a whole?",
+    questionText: "How important do you think this study is for the society as a whole?",
     minLabel: "Not at all",
     maxLabel: "Completely",
   },
