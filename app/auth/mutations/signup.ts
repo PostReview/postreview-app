970d799a3797933d387382aef996c9f84ddbb9c5
Nami Sunami <naoyuki.sunami@gmail.com>
2021-10-27 07:32:03 -0400

    add handle to blitz default signup

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -6,8 +6,8 @@ import { Role } from "types"
 export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
-    data: { email: email.toLowerCase().trim(), hashedPassword, role: "USER" },
-    select: { id: true, name: true, email: true, role: true },
+    data: { email: email.toLowerCase().trim(), handle: email, hashedPassword, role: "USER" },
+    select: { id: true, handle: true, displayName: true, email: true, role: true },
   })
 
   await ctx.session.$create({ userId: user.id, role: user.role as Role })
