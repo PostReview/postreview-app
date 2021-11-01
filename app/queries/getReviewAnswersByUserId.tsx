a4b69da4b84d5a17702cf9add6527149657c1595
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-01 07:11:49 -0400

    formatting

--- app/queries/getReviewAnswersByUserId.tsx
+++ app/queries/getReviewAnswersByUserId.tsx
@@ -3,7 +3,13 @@ import db from "db"
 export default async function getReviewAnswersByUserId(props) {
   const { currentUserId } = props
   return await db.article.findMany({
-    where: { review: { every: { writtenBy: currentUserId } } },
+    where: {
+      review: {
+        every: {
+          userId: currentUserId,
+        },
+      },
+    },
     include: {
       review: {
         include: {
