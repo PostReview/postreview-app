2790b4146813eb0c0a03326e41fa34d20f0258cc
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-11 07:45:06 -0500

    Hero layout changes

--- app/core/components/Hero.tsx
+++ app/core/components/Hero.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <>
-      <div className="px-6 md:px-12 lg:px-24 my-28 flex flex-col items-center">
-        <div className="text-3xl md:text-5xl lg:text-7xl">
+    <div className="flex flex-col justify-evenly items-center bg-purple-300 py-14  w-screen">
+      <div className="text-3xl md:text-5xl lg:text-7xl">
         Post your review, post-publication.
       </div>
       <div className="text-xl text-pr-blue mt-4">
@@ -12,21 +11,19 @@ export const Hero = () => {
       </div>
       <div
         id="action-container"
-          className="my-12"
+        className="mb-6 mt-12"
+      >
+        <input
+          type="text"
+          className="rounded-full placeholder-gray-500 px-12 bg-gray-100 h-8"
+          placeholder="Article title / DOI"
+        />
+        <button
+          className="bg-blue-400 mx-2 rounded-full h-8 w-auto px-4 text-white"
         >
-          <input
-            type="text"
-            className="rounded-full placeholder-gray-500 px-12 bg-gray-100 h-8"
-            placeholder="Article title / DOI"
-          />
-          <button
-            className="bg-blue-400 mx-2 rounded-full h-8 w-auto px-4 text-white"
-          >
-            Start Browsing
-          </button>
-        </div>
+          Start Browsing
+        </button>
       </div>
-
-    </>
+    </div>
   )
 }
