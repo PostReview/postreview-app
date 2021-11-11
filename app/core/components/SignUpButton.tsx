0ea7e19cec8004ccd08043002567839829a25e9d
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-11 08:12:12 -0500

    add SignUpButton component

---
+++ app/core/components/SignUpButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+export const SignUpButton = () => {
+  return (
+    <div className="my-14">
+      <Button variant="contained">Sign Up Today</Button>
+    </div>
+  )
+}
