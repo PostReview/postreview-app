b0b27e7a821eb6d53fb0d9c3cdf83f09d5eb2142
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-18 08:06:27 -0500

    add a MUI Styled Rating component

---
+++ app/core/components/RatingTotal.tsx
@@ -0,0 +1,11 @@
+import { styled } from "@mui/material/styles"
+import { Rating } from "@mui/material"
+
+export const RatingTotal = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#f28c6d",
+  },
+  "& .MuiRating-iconHover": {
+    color: "#ff3d47",
+  },
+})
