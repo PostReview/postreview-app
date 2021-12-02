6f04eedff14ed0f65032aa0dcaf572296019fd6e
Nami Sunami <naoyuki.sunami@gmail.com>
2021-12-02 18:24:05 -0500

    update query to get review answers to grouped by users

--- app/queries/getReviewAnswers.tsx
+++ app/queries/getReviewAnswers.tsx
@@ -2,7 +2,24 @@ import db from "db"
 
 export default async function getReviewAnswers(props) {
   const { currentUserId, currentArticleId } = props
-  return await db.reviewAnswers.findMany({
-    where: { userId: currentUserId, articleId: currentArticleId },
+  // I should update the query so that we are reading the reviews nested within users
+  return await db.user.findMany({
+    where: {
+      review: {
+        every: {
+          articleId: currentArticleId,
+        },
+      },
+    },
+    select: {
+      id: true,
+      displayName: true,
+      handle: true,
+      review: true,
+    },
   })
+  // return await db.reviewAnswers.findMany({
+  //   where: { userId: currentUserId, articleId: currentArticleId },
+  //   include: { writtenBy: { select: { handle: true, displayName: true } } },
+  // })
 }
