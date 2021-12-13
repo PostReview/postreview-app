60fb04e1aac2680cf1b438ee4645311b592f1ea6
Nami Sunami <naoyuki.sunami@gmail.com>
2021-12-13 08:22:01 -0500

    add empty state when users has no reviews

--- app/core/components/ReviewList.tsx
+++ app/core/components/ReviewList.tsx
@@ -12,25 +12,30 @@ export const ReviewList = (prop) => {
     currentArticleId: article?.id,
   })
 
-  const currentUserReview = usersWithReview.filter((user) => user.id == currentUser?.id)
+  const currentUserReview = usersWithReview.find((user) => user.id == currentUser?.id)
+  const currentUserHasReview = currentUserReview?.review.length
   const otherUserReview = usersWithReview.filter((user) => user.id != currentUser?.id)
 
   return (
     <>
       <div id="reviews-container" className="max-w-4xl">
-        <div className="border-b m-6 text-2xl">
-          <h1>Your Rating</h1>
-        </div>
-        <div id="your-review-wrapper" className="flex flex-col items-center">
-          {currentUserReview.map((user) => (
-            <Review
-              key={user.id}
-              displayName={user.handle}
-              handle={user.handle}
-              reviews={user.review}
-              userIcon={user.icon}
-            />
-          ))}
+        <div id="your-rating-wrapper" className="mt-6 mb-6">
+          <div className="border-b m-6 text-2xl">
+            <h1>Your Rating</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            {currentUserHasReview ? (
+              <Review
+                key={currentUserReview.id}
+                displayName={currentUserReview.handle}
+                handle={currentUserReview.handle}
+                reviews={currentUserReview.review}
+                userIcon={currentUserReview.icon}
+              />
+            ) : (
+              <div className="m-20">Submit your review</div>
+            )}
+          </div>
         </div>
         <div className="border-b m-6 text-2xl">
           <h1>All Ratings</h1>
