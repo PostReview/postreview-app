3dc4e7c074a633baf6da719025a12af28f45b0cc
Nami Sunami <naoyuki.sunami@gmail.com>
2022-01-24 07:55:25 -0500

    Avoid importing from another seed file

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,5 +1,4 @@
 import db from "./index"
-import { reviewQuestions } from "./reviewQuestions"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -15,7 +14,53 @@ const seed = async () => {
 
   // Seed review questions
   await db.reviewQuestions.createMany({
-    data: reviewQuestions,
+    data: [
+      {
+        questionId: 1,
+        questionCategory: "Research Question",
+        questionText: "Do you find the research question worthwhile?",
+        minValue: 1,
+        maxValue: 5,
+        minLabel: "Not at all",
+        maxLabel: "Completely",
+      },
+      {
+        questionId: 2,
+        questionCategory: "Design",
+        questionText: "Do you find the way the study was conducted adequate?",
+        minValue: 1,
+        maxValue: 5,
+        minLabel: "Not at all",
+        maxLabel: "Completely",
+      },
+      {
+        questionId: 3,
+        questionCategory: "Findings",
+        questionText: "Do you consider the findings sufficient to support the authors' claims?",
+        minValue: 1,
+        maxValue: 5,
+        minLabel: "Not at all",
+        maxLabel: "Completely",
+      },
+      {
+        questionId: 4,
+        questionCategory: "Interpretation",
+        questionText: "Do you find the authors' explanation plausible?",
+        minValue: 1,
+        maxValue: 5,
+        minLabel: "Not at all",
+        maxLabel: "Completely",
+      },
+      {
+        questionId: 5,
+        questionCategory: "Significance",
+        questionText: "How important do you think this study is for the society as a whole?",
+        minValue: 1,
+        maxValue: 5,
+        minLabel: "Not at all",
+        maxLabel: "Completely",
+      },
+    ],
   })
 
   // Seed users
