5b64bee20211d75d6a26c97dde44962135a5410f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-01-31 19:17:44 -0500

    Remove rate a paper from profile empty state

--- app/core/components/MyReviewsEmptyState.tsx
+++ app/core/components/MyReviewsEmptyState.tsx
@@ -4,18 +4,15 @@ import PostAddIcon from "@mui/icons-material/PostAdd"
 export const MyReviewsEmptyState = () => {
   return (
     <div>
-      <div className="text-gray-500">No ratings submitted yet.</div>
-
       <div
-        className="bg-gray-50 m-6 p-4 border-gray-600 border-2 border-dashed
-    flex flex-col  max-w-5xl"
+        className="bg-gray-50 m-6 p-4 border-gray-500 border-2 border-dashed
+    flex flex-col  max-w-5xl h-32 justify-center"
       >
-        <a href="#" className="self-center">
-          <div className="flex flex-col items-center">
-            <PostAddIcon fontSize="large" color="action" />
-            <div id="rate-paper-container">Rate a paper</div>
+        <div className="flex flex-col items-center">
+          <div id="rate-paper-container" className="text-gray-500">
+            No ratings have been submitted yet
           </div>
-        </a>
+        </div>
       </div>
     </div>
   )
