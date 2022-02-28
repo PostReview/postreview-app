58a88b5e3ed2644d2e8809ecc1dabe5ce18cc4cc
Nami Sunami <naoyuki.sunami@gmail.com>
2022-02-28 08:36:59 -0500

    Move the sign up button to the bottom #111

--- app/core/components/SignUpButton.tsx
+++ app/core/components/SignUpButton.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@mui/material"
 import React from "react"
+import { HeaderUserButton } from "./HeaderUserButton"
 
 export const SignUpButton = () => {
   return (
-    <div className="my-14">
-      <Button variant="contained">Sign Up Today</Button>
+    <div className="my-24">
+      <HeaderUserButton />
     </div>
   )
 }
