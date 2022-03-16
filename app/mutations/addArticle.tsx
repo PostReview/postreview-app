17601289e31b9577465def28d920b73e4d3cf561
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-16 08:25:01 -0400

    Update Algolia for every new article

--- app/mutations/addArticle.tsx
+++ app/mutations/addArticle.tsx
@@ -1,8 +1,13 @@
 import db from "db"
 import { Ctx, AuthorizationError } from "blitz"
 import * as z from "zod"
+import algoliasearch from "algoliasearch"
 
-// schema validation
+// Initialize Algolia
+const client = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_API_ADMIN_KEY!)
+const index = client.initIndex(`${process.env.ALGOLIA_PREFIX}_articles`)
+
+// Vaidate data
 const AddArticle = z.object({
   title: z.string(),
   doi: z.string(),
@@ -21,6 +26,26 @@ export default async function addArticle(input: z.infer<typeof AddArticle>, ctx:
   }
 
   const article = await db.article.create({ data })
+
+  const addedArticle = await db.article.findFirst({
+    where: {
+      doi: data.doi,
+    },
+  })
+
+  // Save the metadata to Algolia
+  await index.saveObject({
+    objectID: addedArticle?.id,
+    name: addedArticle?.title,
+    doi: addedArticle?.doi,
+    authors: addedArticle?.authorString,
+    publishedYear: addedArticle?.publishedYear,
+    journal: addedArticle?.journal,
+    createdAt: addedArticle?.createdAt,
+    createdAt_timestamp: addedArticle?.createdAt.valueOf(),
+    updatedAt: addedArticle?.updatedAt,
+    updatedAt_timestamp: addedArticle?.updatedAt.valueOf(),
+  })
 
   return article
 }
