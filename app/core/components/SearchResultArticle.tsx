8ffddc14ed73a78d87ba1cc90700de92b6d55573
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-22 08:18:55 -0400

    Add search results template

---
+++ app/core/components/SearchResultArticle.tsx
@@ -0,0 +1,16 @@
+const SearchResultArticle = ({ item }) => {
+  return (
+    <>
+      <div className="my-1 mx-1 flex">
+        <div className="mr-2">{item.name}</div>
+        <div>
+          <p className="text-md font-normal leading-4 text-gray-500 dark:text-gray-400">
+            {item.authors} ({item.publishedYear})
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SearchResultArticle
