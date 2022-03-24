42e4860de3c757024a03b6c53ef96939dac5fe7f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-24 08:19:01 -0400

    Remove visions from the landing page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,26 +1,16 @@
 import React, { Suspense } from "react"
-import { useRouter, useSession, BlitzPage, useMutation, Routes } from "blitz"
+import { useRouter, useSession, BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
 // Components
 import Header from "../core/components/Header"
-import EnterDOI from "../core/components/EnterDOI"
 import { Footer } from "app/core/components/Footer"
 import { Hero } from "app/core/components/Hero"
 import { HowItWorks } from "app/core/components/HowItWorks"
-import { Features } from "app/core/components/Features"
 import { SignUpButton } from "app/core/components/SignUpButton"
-import { Visions } from "app/core/components/Visions"
 import ArticleList from "app/core/components/ArticleList"
-
-const LOCAL_STORAGE_KEY = "doiResolver"
-
-// useEffect to update the local stroage
-/*
- * This file is just for a pleasant getting started page for your new app.
- * You can delete everything in here and start from scratch if you like.
- */
+import { Features } from "app/core/components/Features"
 
 const UserInfo = () => {
   const currentUser = useCurrentUser()
@@ -38,10 +28,9 @@ const UserInfo = () => {
     return (
       <>
         <Hero />
-        <Features />
         <HowItWorks />
-        <Visions />
         <ArticleList />
+        <Features />
         <SignUpButton />
       </>
     )
@@ -63,9 +52,6 @@ const Home: BlitzPage = () => {
     </div>
   )
 }
-
-// Default <Values></Values>
-const defaultArticles = []
 
 Home.suppressFirstRenderFlicker = true
 Home.getLayout = (page) => <Layout title="Home">{page}</Layout>
