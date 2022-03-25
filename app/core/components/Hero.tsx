c99edaeb799e04b2c8d162cb48abce52ad336761
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-25 08:09:27 -0400

    Remove period from the title

--- app/core/components/Hero.tsx
+++ app/core/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Hero = () => {
   return (
     <div className="flex flex-col items-center pt-24 pb-12 px-4 w-screen bg-slate-50">
-      <div className="text-5xl lg:text-6xl font-bold">Post your review, post-publication.</div>
+      <div className="text-5xl lg:text-6xl font-bold">Post your review, post-publication</div>
       <div className="text-xl text-slate-700 mt-6">
         Share your thoughts with those who care—Opinions in science have never been this accessible
       </div>
