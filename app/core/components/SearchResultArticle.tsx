20950f003a5ff76bdde06271277e8209deed8785
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-26 06:38:44 -0400

    Highlight hits

--- app/core/components/SearchResultArticle.tsx
+++ app/core/components/SearchResultArticle.tsx
@@ -1,8 +1,10 @@
-const SearchResultArticle = ({ item }) => {
+const SearchResultArticle = ({ item, components }) => {
   return (
     <>
       <div className="my-1 mx-1 flex">
-        <div className="mr-2">{item.name}</div>
+        <div className="mr-2">
+          {components ? <components.Highlight hit={item} attribute="name" /> : undefined}
+        </div>
         <div>
           <p className="text-md font-normal leading-4 text-gray-500 dark:text-gray-400">
             {item.authors} ({item.publishedYear})
