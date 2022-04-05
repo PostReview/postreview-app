7865f13caacb9fc699a89d17ca4f39301eb59c8f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-05 08:16:54 -0400

    Remove unused code

--- app/core/components/EnterDOI.tsx
+++ app/core/components/EnterDOI.tsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react"
-import { invoke, useMutation, useRouter } from "blitz"
-import addArticle from "../../mutations/addArticle"
-import getArticleByDoi from "../../queries/getArticleByDoi"
-import axios from "axios"
+import { useRouter } from "blitz"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
-
-import { v4 as uuidv4 } from "uuid"
-import { Button, Dialog } from "@mui/material"
+import { Dialog } from "@mui/material"
 import { Autocomplete } from "./Autocomplete"
 import { getAlgoliaResults } from "@algolia/autocomplete-js"
 import algoliasearch from "algoliasearch"
@@ -21,66 +16,8 @@ const searchClient = algoliasearch(
 
 export default function EnterDOI() {
   const currentUser = useCurrentUser()
-  const defaultDoi = ""
-  const [doi, setDoi] = useState(defaultDoi)
   const [isPaperPopupOpen, setPaperPopupOpen] = useState(false)
-
-  const [addArticleMutation] = useMutation(addArticle)
-
-  async function getArticleMetadata() {
-    if (!doi) return handleArticleNotFound()
-    try {
-      const doiURL = "https://api.crossref.org/works/" + doi
-      const response = await axios.get(doiURL)
-      const newArticleMetadata = response.data.message
-      return newArticleMetadata
-    } catch {
-      handleArticleNotFound()
-    }
-  }
-
-  async function parseArticleMetadata(newArticleMetadata) {
-    const newArticle = {
-      id: uuidv4(),
-      title: newArticleMetadata.title[0],
-      doi: newArticleMetadata.DOI,
-      publishedYear: newArticleMetadata.created["date-parts"][0][0],
-      journal: newArticleMetadata["container-title"][0],
-      addedBy: currentUser?.handle,
-      addedById: currentUser?.id,
-      authorString: newArticleMetadata.author
-        .map((author, i, authors) => {
-          if (authors.length - 1 === i) {
-            /* If last author, do not print the semicolon */
-            return `${author.family}, ${author.given}`
-          } else {
-            return `${author.family}, ${author.given}; `
-          }
-        })
-        .join(""),
-    }
-    return newArticle
-  }
-
   const router = useRouter()
-  async function handleArticleAdd() {
-    const newArticleMetadata = await getArticleMetadata()
-    if (!newArticleMetadata) return null
-    const newArticle = await parseArticleMetadata(newArticleMetadata)
-    // Is article already in the database?
-    const existingArticle = await invoke(getArticleByDoi, newArticle.doi)
-    if (existingArticle) return router.push("/articles/" + existingArticle.id)
-    // push to database
-    const addedArticle = await invoke(addArticleMutation, { ...newArticle })
-    // go to the added article
-    router.push(`/articles/${addedArticle.id}`)
-    // add authors - implement with Nested Writes in the future
-    // https://www.prisma.io/docs/concepts/components/prisma-client/relation-queries#nested-writes
-  }
-
-  function handleArticleNotFound() {
-    window.alert("Article not found")
-  }
 
   return (
     <div className="m-1 rounded-md flex flex-row items-center flex-grow max-w-7xl justify-end">
@@ -133,9 +70,9 @@ export default function EnterDOI() {
       {currentUser ? (
         <button
           className="bg-indigo-500 hover:bg-indigo-700 text-white font-semibold mx-2 rounded-md text-sm w-32 py-3"
-        onClick={() => setPaperPopupOpen(true)}
-      >
-        + Add Paper
+          onClick={() => setPaperPopupOpen(true)}
+        >
+          + Add Paper
         </button>
       ) : (
         ""
