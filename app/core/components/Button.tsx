29ff58921fa14a4a5dbfac589872a29e50c66161
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-06 08:15:29 -0400

    Update button component to accept additional class

--- app/core/components/Button.tsx
+++ app/core/components/Button.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 
 export const Button = (props) => {
-  const { className } = props
+  const { additionalClass } = props
   return (
     <button
-      className={`bg-indigo-500 hover:bg-indigo-700 text-white mx-2 rounded-md px-3 py-2 ${className}`}
+      className={`bg-indigo-500 hover:bg-indigo-700 text-white mx-2 rounded-md px-3 py-2 whitespace-nowrap ${additionalClass}`}
       {...props}
     >
       {props.children}
