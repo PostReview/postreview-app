cbef13db4dcad31b6cb86c1aac8079599f99689f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-06 07:46:12 -0400

    Use the Button component in the hero

--- app/core/components/Hero.tsx
+++ app/core/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Button } from "./Button"
 
 export const Hero = () => {
   return (
@@ -8,12 +9,7 @@ export const Hero = () => {
         Share your thoughts with those who care—Opinions in science have never been this accessible
       </div>
       <div id="action-container" className="my-6">
-        <button
-          className="bg-indigo-500 hover:bg-indigo-700 text-white mx-2 p-2 px-3 border rounded-md"
-          onClick={undefined}
-        >
-          Start Browsing
-        </button>
+        <Button onClick={undefined}>Start Browsing</Button>
       </div>
     </div>
   )
