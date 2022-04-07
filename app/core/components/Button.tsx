852d8ecb17cf8eb17e46567020b69cdc8741efe3
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-07 08:07:45 -0400

    Add loading state to Button

--- app/core/components/Button.tsx
+++ app/core/components/Button.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import { FaSpinner } from "react-icons/fa"
 
 export const Button = (props) => {
-  const { additionalClass, type } = props
+  const { additionalClass, type, loading } = props
   if (type == "cancel")
     return (
       <button
@@ -18,6 +19,16 @@ export const Button = (props) => {
         {...props}
       >
         {props.children}
+      </button>
+    )
+  if (loading)
+    return (
+      <button
+        className={`bg-slate-400 text-white mx-2 rounded-md px-3 py-2 whitespace-nowrap flex items-center justify-center ${additionalClass}`}
+        {...props}
+      >
+        <div className="opacity-0">{props.children}</div>
+        <FaSpinner className="absolute animate-spin" />
       </button>
     )
   return (
