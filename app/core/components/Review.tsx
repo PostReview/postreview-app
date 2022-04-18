d27eb93cdf3db15a02ea2f7893c4619718a8c82b
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-18 08:35:24 -0400

    Fix kebab-case error

--- app/core/components/Review.tsx
+++ app/core/components/Review.tsx
@@ -19,7 +19,7 @@ export const Review = (props) => {
         <Tooltip title={tooltipText} placement="top" arrow>
           <Button id="user-avatar" className="focus:outline-none">
             {isAnonymous ? (
-              <Avatar alt={submittedBy} sx={{ "background-color": "rgb(217 119 6)" }}>
+              <Avatar alt={submittedBy} sx={{ backgroundColor: "rgb(217 119 6)" }}>
                 <Icon icon="mdi:incognito" className="" />
               </Avatar>
             ) : (
