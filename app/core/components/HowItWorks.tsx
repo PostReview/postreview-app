f81bc0e0d422c58eb79b955f895a982dee978def
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-21 22:24:05 -0400

    Increase the max width & center-align headings

--- app/core/components/HowItWorks.tsx
+++ app/core/components/HowItWorks.tsx
@@ -8,35 +8,48 @@ export const HowItWorks = () => {
   return (
     <div
       id="how-it-works-container"
-      className="flex flex-col items-start min-h-screen justify-evenly my-12 max-w-2xl m-12"
+      className="flex flex-col items-center min-h-screen justify-evenly my-12 max-w-4xl m-12 mb-24 space-y-32"
     >
+      <div className="text-4xl font-bold">How it works</div>
+      <div>
         <div className="text-center mb-6 text-2xl font-bold ">Enter the title of the paper</div>
         <div className="max-w-xl">
           <Image
             src={enterTitleScreenshot}
             alt="A card showing an article with ratings across users"
           />
+        </div>
       </div>
+      <div>
+        <div className="text-center mb-6 text-2xl font-bold">Rate the paper</div>
         <div className="max-w-xl">
-        <Image
-          src={popupReviewScreenshot}
-          alt="A screen recording where the user is choosing ratings for the article"
-        />
+          <Image
+            src={popupReviewScreenshot}
+            alt="A screen recording where the user is choosing ratings for the article"
+          />
+        </div>
       </div>
+      <div>
         <div className="text-center mb-6 text-2xl font-bold">
           You can browse others&apos; ratings
         </div>
         <div className="max-w-full">
-        <Image
+          <Image
             src={reviewAnswersScreenshot}
-          alt="A card showing ratings for the article by one user"
-        />
+            alt="A card showing ratings for the article by one user"
+          />
+        </div>
       </div>
+      <div>
         <div className="text-center mt-14 mb-6 text-2xl font-bold">
           Everyone&apos;s ratings, combined
         </div>
-      <div className="self-center">
-        <Image src={articleScreenshot} alt="A card showing an article with ratings across users" />
+        <div className="self-center">
+          <Image
+            src={articleScreenshot}
+            alt="A card showing an article with ratings across users"
+          />
+        </div>
       </div>
     </div>
   )
