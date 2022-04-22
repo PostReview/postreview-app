4d25aaaacbc35f88fe88ed01c9eac322f52f7bda
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-21 21:42:37 -0400

    Whe not logged in, hide search bar on root

--- app/core/components/Header.tsx
+++ app/core/components/Header.tsx
@@ -2,10 +2,11 @@ import React from "react"
 import { HeaderUserButton } from "./HeaderUserButton"
 import EnterDOI from "./EnterDOI"
 import { useCurrentUser } from "../hooks/useCurrentUser"
-import { Link } from "blitz"
+import { Link, useRouter } from "blitz"
 
 export default function Header() {
   const currentUser = useCurrentUser()
+  const router = useRouter()
   return (
     <>
       <header className="bg-gray-100 flex flex-row items-center h-16">
@@ -13,7 +14,7 @@ export default function Header() {
           <Link href="/">PostReview</Link>
         </div>
         <div id="search-bar-container" className="flex flex-grow justify-center">
-          {currentUser && <EnterDOI />}
+          {currentUser ? <EnterDOI /> : router.pathname != "/" ? <EnterDOI /> : ""}
         </div>
         <div id="buttons-container" className="mr-4 flex flex-row">
           <HeaderUserButton />
