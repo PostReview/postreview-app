f85a4bf3909dafb41e574dcc97a4febaae33be72
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-27 08:43:39 -0400

    Signup mutation

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -3,10 +3,10 @@ import db from "db"
 import { Signup } from "app/auth/validations"
 import { Role } from "types"
 
-export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
+export default resolver.pipe(resolver.zod(Signup), async ({ email, handle, password }, ctx) => {
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
-    data: { email: email.toLowerCase().trim(), handle: email, hashedPassword, role: "USER" },
+    data: { email: email.toLowerCase().trim(), handle: handle, hashedPassword, role: "USER" },
     select: { id: true, handle: true, displayName: true, email: true, role: true },
   })
 
