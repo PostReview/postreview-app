ebcf0286e1b987e3ea82e64d3453bc632474d804
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-27 20:29:21 -0400

    Use postmark

--- mailers/forgotPasswordMailer.ts
+++ mailers/forgotPasswordMailer.ts
@@ -4,7 +4,6 @@
  * and then export it. That way you can import here and anywhere else
  * and use it straight away.
  */
-import previewEmail from "preview-email"
 
 type ResetPasswordMailer = {
   to: string
@@ -16,30 +15,29 @@ export function forgotPasswordMailer({ to, token }: ResetPasswordMailer) {
   const origin = process.env.APP_ORIGIN || process.env.BLITZ_DEV_SERVER_ORIGIN
   const resetUrl = `${origin}/reset-password?token=${token}`
 
-  const msg = {
-    from: "TODO@example.com",
-    to,
-    subject: "Your Password Reset Instructions",
-    html: `
-      <h1>Reset Your Password</h1>
-      <h3>NOTE: You must set up a production email integration in mailers/forgotPasswordMailer.ts</h3>
+  let postmark = require("postmark")
+  const serverToken = process.env.POSTMARK_TOKEN
+  let client = new postmark.ServerClient(serverToken)
 
-      <a href="${resetUrl}">
-        Click here to set a new password
-      </a>
-    `,
+  const msg = {
+    From: "hello@postreview.org",
+    To: to,
+    TemplateAlias: "password-reset",
+    TemplateModel: {
+      product_url: process.env.PUBLIC_URL,
+      product_name: "PostReview",
+      action_url: resetUrl,
+      operating_system: "operating_system_Value",
+      browser_name: "browser_name_Value",
+      support_url: "hello@postreview.org",
+      company_name: "PostReview",
+      company_address: "",
+    },
   }
 
   return {
     async send() {
-      if (process.env.NODE_ENV === "production") {
-        // TODO - send the production email, like this:
-        // await postmark.sendEmail(msg)
-        throw new Error("No production email implementation in mailers/forgotPasswordMailer")
-      } else {
-        // Preview email in the browser
-        await previewEmail(msg)
-      }
+      await client.sendEmailWithTemplate(msg)
     },
   }
 }
