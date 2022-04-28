15294681c6948fa86148a8fdcaa08ae9aa506b62
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-27 21:41:58 -0400

    Change the app title

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -57,6 +57,6 @@ const Home: BlitzPage = () => {
 }
 
 Home.suppressFirstRenderFlicker = true
-Home.getLayout = (page) => <Layout title="Home">{page}</Layout>
+Home.getLayout = (page) => <Layout title="PostReview">{page}</Layout>
 
 export default Home
