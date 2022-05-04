ada1bafbc6d87ded1aaa5f065b56c1dd3dbd39d7
Nami Sunami <naoyuki.sunami@gmail.com>
2022-05-04 08:27:27 -0400

    Send an email upon signup

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -3,6 +3,8 @@ import db from "db"
 import { Signup } from "app/auth/validations"
 import { Role } from "types"
 import algoliasearch from "algoliasearch"
+import { generateCode } from "../verify-email-utils"
+import sendEmailWithTemplate from "mailers/sendEmailWithTemplate"
 
 // Initialize Algolia
 const client = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_API_ADMIN_KEY!)
@@ -20,12 +22,37 @@ export default resolver.pipe(resolver.zod(Signup), async ({ email, handle, passw
   // Get added user
   const addedUser = await db.user.findFirst({ where: { handle: handle } })
 
+  // Save to Algolia
   await index.saveObject({
     objectID: addedUser?.id,
     name: addedUser?.handle,
     displayName: addedUser?.displayName,
     icon: addedUser?.icon,
   })
+
+  // Send a password verification email
+  const emailCode = await generateCode(hashedPassword)
+  const origin = process.env.APP_ORIGIN || process.env.BLITZ_DEV_SERVER_ORIGIN
+
+  const msg = {
+    From: "hello@postreview.org",
+    To: user.email,
+    TemplateAlias: "welcome",
+    TemplateModel: {
+      product_url: process.env.PUBLIC_URL,
+      product_name: "PostReview",
+      name: addedUser?.handle,
+      expire_in: 30,
+      action_url: `${origin}/verify-email/${emailCode}?userId=${user.id}`,
+      operating_system: "operating_system_Value",
+      browser_name: "browser_name_Value",
+      support_url: "hello@postreview.org",
+      company_name: "PostReview",
+      company_address: "",
+    },
+  }
+
+  await sendEmailWithTemplate(msg)
 
   return user
 })
