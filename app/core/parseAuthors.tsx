e483f8be63f3805e57a0a65f0120a1f5373a49f2
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-10 17:02:07 -0400

    Handle undefined author names

--- app/core/parseAuthors.tsx
+++ app/core/parseAuthors.tsx
@@ -1,10 +1,8 @@
 // Parse author array from Cross Ref
 // to one author string, formatted in the APA-like style
 export const parseAuthors = (author, i, authors) => {
-  if (authors.length - 1 === i) {
-    /* If last author, do not print the semicolon */
-    return `${author.family}, ${author.given}`
-  } else {
-    return `${author.family}, ${author.given}; `
-  }
+  // If the author name is a string "undefined", set as undefined
+  if (author.given === "undefined" || author.family === "undefined") return undefined
+
+  return `${author.family}, ${author.given}`
 }
