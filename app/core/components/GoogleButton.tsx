669cd083cfa52f5748dd09f548a897782165b459
jazellemaira <jazellemaira@gmail.com>
2022-08-11 18:47:07 -0400

    Put a custom "sign in with google" buttton

--- app/core/components/GoogleButton.tsx
+++ app/core/components/GoogleButton.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { Image } from "blitz"
-import googleLogo from "public/google.png"
+import googleLogo from "public/google-custom.png"
 
 export default function GoogleButton() {
   return (
     <>
       <a href="/api/auth/google">
-        <Image src={googleLogo} alt="Sign-in with Google" height={48} width={200} />
+        <Image src={googleLogo} alt="Sign-in with Google" height={106 / 2} width={374 / 2} />
       </a>
     </>
   )
