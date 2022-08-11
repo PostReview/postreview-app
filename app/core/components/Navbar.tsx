7d1d671109a8b0baf0089849cf3abc018cd0f850
jazellemaira <jazellemaira@gmail.com>
2022-08-10 20:25:27 -0400

    Change logo to button

--- app/core/components/Navbar.tsx
+++ app/core/components/Navbar.tsx
@@ -11,9 +11,9 @@ export default function Navbar() {
   return (
     <>
       <nav className="bg-gradient-to-r from-gray-darkest to-gray-dark flex flex-row items-center h-16">
-        <div className="object-none h-20 w-20 p-2">
+        <button className="object-none h-20 w-20 p-2">
           <Link href="/"><Image src={postReviewIcon}></Image></Link>
-        </div>
+        </button>
         <div id="search-bar-container" className="flex flex-grow justify-center">
           {currentUser ? <EnterDOI /> : router.pathname != "/" ? <EnterDOI /> : ""}
         </div>
