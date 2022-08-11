fcf4d7578ff7af2b746fc72b1abf35d6ebe5ca5c
jazellemaira <jazellemaira@gmail.com>
2022-08-11 00:10:38 -0400

    Update hero background and button hover state

--- app/core/components/Hero.tsx
+++ app/core/components/Hero.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "blitz"
 import React from "react"
-import { Button } from "./Button"
 import EnterDOI from "./EnterDOI"
 
 export const Hero = () => {
@@ -8,7 +7,7 @@ export const Hero = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center pt-24 pb-12 px-4 w-full bg-gray-light dark:bg-gray-darkest">
+      <div className="flex flex-col items-center pt-24 pb-12 px-4 w-full bg-gray-light dark:bg-gray-dark">
         <div className="text-5xl lg:text-6xl font-bold text-gray-darkest dark:text-white">PostReview</div>
         <div className="text-xl text-center text-gray-darkest dark:text-gray-light mt-6">
           Share your thoughts and be part of a collective knowledge
@@ -16,7 +15,7 @@ export const Hero = () => {
         <div id="action-container" className="my-6 flex flex-col w-full lg:w-1/2 bg-gray-darkest dark:bg-black">
           <EnterDOI />
         </div>
-        <div id="action-container" className="text-xl text-green rounded-lg bg-gray-medium dark:bg-gray-medium hover:bg-gray-dark">
+        <div id="action-container" className="text-xl text-green rounded-lg bg-gray-medium dark:bg-gray-medium hover:bg-gray-darkest">
           <button className="mx-4 my-4" onClick={() => router.push("signup")}>
             GET STARTED
           </button></div>
