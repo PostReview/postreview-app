b0d58455df437c3267c71f0f6690da4d626733de
jazellemaira <jazellemaira@gmail.com>
2022-08-12 16:10:22 -0400

    Change margin for main flex

--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -37,7 +37,7 @@ const SignupPage: BlitzPage = () => {
         />
       </div>
       <h1 className="mt-0 text-center text-4xl font-bold my-4 text-gray-darkest dark:text-white">Join PostReview</h1>
-      <main className="mb-16 sm:mb-80 sm:mx-40 flex-grow flex flex-col items-center justify-center bg-gray-light dark:bg-gray-dark">
+      <main className="mb-16 sm:mb-80 sm:mx-96 flex-grow flex flex-col items-center justify-center bg-gray-light dark:bg-gray-dark">
         <div className="flex flex-col items-center py-6 px-2">
           <Formik
             initialValues={{ email: "", password: "", passwordVerify: "", handle: "" }}
