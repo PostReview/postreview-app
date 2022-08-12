e47dc875e285d3b4d4cbd405243a793518dc4381
jazellemaira <jazellemaira@gmail.com>
2022-08-12 14:04:01 -0400

    Add Google button variations

--- app/core/components/GoogleButton.tsx
+++ app/core/components/GoogleButton.tsx
@@ -1,8 +1,15 @@
 import React from "react"
 import { Image } from "blitz"
-import googleLogo from "public/google-custom.png"
+import googleLogoSignIn from "public/google-custom-sign-in.png"
+import googleLogoSignUp from "public/google-custom-sign-up.png"
 
-export default function GoogleButton() {
+
+export default function GoogleButton(props = { type: "sign-in" }) {
+
+  const { type } = props
+
+  const googleLogo = type === "sign-in" ? googleLogoSignIn : type === "sign-up" ? googleLogoSignUp : googleLogoSignIn
+
   return (
     <>
       <a href="/api/auth/google">
