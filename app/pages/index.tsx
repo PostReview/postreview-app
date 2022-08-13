ee2c48c691f184f9e04f626094210e0e60f643c8
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-13 16:43:18 -0400

    Use session in the index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react"
 import { useRouter, useSession, BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
 // Components
 import Navbar from "../core/components/Navbar"
@@ -12,12 +11,10 @@ import ArticleList from "app/core/components/ArticleList"
 import { Features } from "app/core/components/Features"
 
 const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  // Redirect
   const router = useRouter()
   const session = useSession()
 
-  if (currentUser) {
+  if (session.userId) {
     return (
       <>
         <ArticleList />
