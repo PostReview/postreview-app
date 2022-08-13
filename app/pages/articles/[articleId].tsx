d85e4de41ded827d23fab00f91bba1035487865c
jazellemaira <jazellemaira@gmail.com>
2022-08-13 19:31:36 -0400

    Remove the "rate this paper" button

--- app/pages/articles/[articleId].tsx
+++ app/pages/articles/[articleId].tsx
@@ -65,21 +65,6 @@ const ArticleDetails = () => {
     setIsReviewDialogOpen(true)
   }
 
-  const ActionButton = ({ state }) => {
-    if (state == "edit")
-      return (
-        <Button variant="contained" onClick={openReviewDialog}>
-          Edit Your Rating
-        </Button>
-      )
-    if (state == "submit")
-      return (
-        <Button variant="contained" onClick={openReviewDialog}>
-          Rate This Paper
-        </Button>
-      )
-  }
-
   // For rendering question summary scores
   const [questionCategories] = useQuery(getQuestionCategories, undefined)
   const [articleScores] = useQuery(getArticleScoresById, {
@@ -200,15 +185,16 @@ const ArticleDetails = () => {
             </div>
           </>
         }
-        <div className="m-16">
-          <button className="mb-12 px-4 py-4 text-xl text-green rounded-lg bg-black/50 hover:bg-gray-darkest dark:bg-gray-medium dark:hover:bg-black/40"
-            onClick={openReviewDialog}
-          >
-            Add review
-          </button>
-        </div>
+        {!userHasReview &&
+          <div className="m-16">
+            <button className="px-4 py-4 text-xl text-green rounded-lg bg-black/50 hover:bg-gray-darkest dark:bg-gray-medium dark:hover:bg-black/40"
+              onClick={openReviewDialog}
+            >
+              Add review
+            </button>
+          </div>}
         {articleHasReview &&
-          <ReviewList article={article} ActionButton={ActionButton} />
+          <ReviewList article={article} ratingScaleMax={ratingScaleMax} />
         }
         <Dialog open={isReviewDialogOpen} onClose={closeReviewDialog}>
           <PopupReview
