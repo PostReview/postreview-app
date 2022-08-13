4cdd0b30bfaf00712fea6af5fda5cfb4450b921f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-13 16:42:45 -0400

    Use session in the nav bar

--- app/core/components/Navbar.tsx
+++ app/core/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { HeaderUserButton } from "./HeaderUserButton"
 import EnterDOI from "./EnterDOI"
-import { useCurrentUser } from "../hooks/useCurrentUser"
-import { Link, useRouter } from "blitz"
+import { Link, useRouter, useSession } from "blitz"
 
 export default function Navbar() {
-  const currentUser = useCurrentUser()
+  const session = useSession()
   const router = useRouter()
+  const isAtRoot = router.pathname === "/"
+
   return (
     <>
       <nav className="bg-gray-100 flex flex-row items-center h-16">
@@ -14,7 +15,11 @@ export default function Navbar() {
           <Link href="/">PostReview</Link>
         </div>
         <div id="search-bar-container" className="flex flex-grow justify-center">
-          {currentUser ? <EnterDOI /> : router.pathname != "/" ? <EnterDOI /> : ""}
+          {session?.userId ? (
+            <EnterDOI session={session} />
+          ) : (
+            !isAtRoot && <EnterDOI session={session} />
+          )}
         </div>
         <div id="buttons-container" className="mr-4 flex flex-row">
           <HeaderUserButton />
