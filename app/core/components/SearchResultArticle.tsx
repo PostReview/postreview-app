86250f447f8bef858d2e265a7690d2da1f2689b5
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-15 19:07:53 -0400

    Render the title and authors vertically in results

--- app/core/components/SearchResultArticle.tsx
+++ app/core/components/SearchResultArticle.tsx
@@ -1,12 +1,13 @@
 const SearchResultArticle = ({ item, components }) => {
   return (
     <>
-      <div className="my-1 mx-1 flex">
+      <div className="my-1 mx-1 flex-col">
         <div className="mr-2">
-          {components ? <components.Highlight hit={item} attribute="name" /> : item.title}
+          {item.title}
+          <components.Highlight hit={item} attribute="name" />
         </div>
         <div>
-          <p className="text-md font-normal leading-4 text-gray-500 dark:text-gray-400">
+          <p className="text-xs text-gray-500 dark:text-gray-400">
             {item?.authors} {item.publishedYear && `(${item.publishedYear})`}
           </p>
         </div>
