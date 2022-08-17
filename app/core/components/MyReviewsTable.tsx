e3818b8a405f276a5e37b2cd9a1da38f0dce1d9b
jazellemaira <jazellemaira@gmail.com>
2022-08-17 13:02:34 -0400

    Change the look of the "reviews table" section

--- app/core/components/MyReviewsTable.tsx
+++ app/core/components/MyReviewsTable.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, useQuery } from "blitz"
 import getQuestionCategories from "app/queries/getQuestionCategories"
-import { FaBook, FaUser } from "react-icons/fa"
+import { FaBarcode, FaBook, FaCrown, FaUser } from "react-icons/fa"
 import { Review } from "app/core/components/Review"
 import { ArticleAction } from "./ArticleAction"
 
@@ -17,25 +17,25 @@ export const MyReviewsTable = (props) => {
         return (
           <div
             key={article.id}
-            className="bg-gray-50 m-6 p-4 border-gray-200 border-2
+            className="bg-gray-light/10 m-6 p-4 border-gray-medium/20 border-2
             flex flex-col  max-w-5xl"
           >
             <div
               id="metadata-container"
               className="mx-4 flex flex-row justify-between items-center"
             >
-              <div id="article-metadata" className="m-2">
+              <div id="article-metadata" className="m-2 text-gray-darkest dark:text-white">
                 <Link href={`/articles/${article.id}`}>
                   <a>
                     <h2 className="font-bold">{article.title}</h2>
                   </a>
                 </Link>
-                <div id="author" className="text-sm text-gray-700">
-                  <FaUser className="inline mr-2" />
+                <div id="author" className="text-sm text-gray-darkest dark:text-white opacity-70">
+                  <FaCrown className="inline mr-2" />
                   {article.authorString} ({article.publishedYear})
                 </div>
-                <div className="text-sm text-violet-600">
-                  <FaBook className="inline mr-2" />
+                <div className="text-sm text-green underline">
+                  <FaBarcode className="inline mr-2" />
                   <a href={`https://doi.org/${article.doi}`} target="_blank" rel="noreferrer">
                     {article.doi}
                   </a>
