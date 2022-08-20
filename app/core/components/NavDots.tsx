914dc9b891a03a82b1257417caf1386ba3aceb5b
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-20 13:57:45 -0400

    Create a component for the navigation dots

---
+++ app/core/components/NavDots.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+const Dot = (props) => {
+  return (
+    <div
+      {...props}
+      className={`${props.selected ? "w-5 h-5" : "w-3 h-3 hover:h-3 hover:w-3"}\
+   ${props.category === "Overall" ? "bg-green" : "bg-white"} rounded-full \
+    hover:cursor-pointer \
+    `}
+    ></div>
+  )
+}
+
+export const NavDots = (props) => {
+  const { searchCategories, setQuestionCategory, questionCategory } = props
+
+  return (
+    <div id="dot-area" className="mx-auto w-72 fixed inset-x-0 bottom-16">
+      <div
+        id="dot-bar"
+        className="px-4 flex flex-row h-12 rounded-3xl bg-gray-dark bg-opacity-80 items-center justify-around"
+      >
+        {searchCategories.map((category) => (
+          <Dot
+            key={category}
+            onClick={() => setQuestionCategory(category)}
+            selected={category === questionCategory}
+            category={category}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
