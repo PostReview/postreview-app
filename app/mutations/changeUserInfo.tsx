3d05887fa82db37f59707e6bb4a4b026451fecc6
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-19 22:32:09 -0400

    Add a DB mutation to change user info

---
+++ app/mutations/changeUserInfo.tsx
@@ -0,0 +1,31 @@
+import db from "db"
+import { Ctx, AuthorizationError } from "blitz"
+import * as z from "zod"
+
+const inputData = z.object({
+  id: z.number(),
+  handle: z.string(),
+  displayName: z.string().optional(),
+  pronoun: z.string().optional(),
+  aboutMe: z.string().optional(),
+  website: z.string().optional(),
+})
+
+export default async function changeUserInfo(input: z.infer<typeof inputData>, ctx: Ctx) {
+  const data = inputData.parse(input)
+  ctx.session.$authorize()
+
+  if (!ctx.session.userId) {
+    throw new AuthorizationError()
+  }
+
+  const { id, ...otherData } = data
+
+  const result = await db.user.update({
+    where: { id: data.id },
+    data: {
+      ...otherData,
+    },
+  })
+  return result
+}
