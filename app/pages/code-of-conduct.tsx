739585ede6abc9d85b12082145e52bdb75f32ad7
jazellemaira <jazellemaira@gmail.com>
2022-08-19 22:49:25 -0400

    Import dependencies

--- app/pages/code-of-conduct.tsx
+++ app/pages/code-of-conduct.tsx
@@ -4,16 +4,13 @@ import { Suspense } from "react"
 import Navbar from "app/core/components/Navbar"
 import { Footer } from "app/core/components/Footer"
 import codeOfConduct from "public/code-of-conduct.png"
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionSummary, {
-  AccordionSummaryProps,
-} from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-
+import * as React from "react"
+import { styled } from "@mui/material/styles"
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp"
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion"
+import MuiAccordionSummary, { AccordionSummaryProps } from "@mui/material/AccordionSummary"
+import MuiAccordionDetails from "@mui/material/AccordionDetails"
+import Typography from "@mui/material/Typography"
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
