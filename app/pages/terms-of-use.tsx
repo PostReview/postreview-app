b59e1645320b7ff1802fdae2d8a3efd8d9085984
jazellemaira <jazellemaira@gmail.com>
2022-08-22 17:23:28 -0400

    Add styling for accordion details

--- app/pages/terms-of-use.tsx
+++ app/pages/terms-of-use.tsx
@@ -40,3 +40,8 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
   },
 }))
 
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}))
+
