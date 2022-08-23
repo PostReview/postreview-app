b133b90ec0280596b596e0b7c1889f93bf22eaaf
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-22 23:01:32 -0400

    Add the close button and change layout

--- app/core/components/ReviewQuestion.tsx
+++ app/core/components/ReviewQuestion.tsx
@@ -34,10 +34,10 @@ export const ReviewQuestion = (props) => {
       m-3
       px-6
       py-2
-      rounded-full"
+      rounded-full relative"
     >
-      <div className="text-center font-bold">{question.questionCategory}</div>
-      <div id="question-header" className="text-center mb-2">
+      <div className="font-bold">{question.questionCategory}</div>
+      <div id="question-header" className="mb-2">
         {question.questionText}
       </div>
       <div
@@ -72,6 +72,16 @@ export const ReviewQuestion = (props) => {
            w-1/3
            p-2"
         ></div>
+        <div
+          id="delete-button"
+          className="absolute text-3xl top-0 right-6 hover:cursor-pointer"
+          onClick={() => {
+            setCurrentAnswer({ ...currentAnswer, response: null })
+            handleRatingChange(question.questionId, null)
+          }}
+        >
+          ×
+        </div>
       </div>
     </div>
   )
