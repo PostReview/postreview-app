8b8d175013e1ebf63a966c7961d528f74167f154
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-22 22:21:29 -0400

    Use absolute path

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -99,7 +99,7 @@ const UserInfo = () => {
         <HowItWorks />
         <button
           className="my-12 px-4 py-4 text-xl text-green rounded-lg bg-black/50 hover:bg-gray-darkest dark:bg-gray-medium dark:hover:bg-black/40"
-          onClick={() => router.push("signup")}
+          onClick={() => router.push("/signup")}
         >
           Sign up now
         </button>
