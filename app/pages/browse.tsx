73adf5b8a06cd4a818c0a3bf6c9d3e0f26fa66b9
jazellemaira <jazellemaira@gmail.com>
2022-08-23 16:14:45 -0400

    Fix the padding and add the nav bar

--- app/pages/browse.tsx
+++ app/pages/browse.tsx
@@ -1,4 +1,5 @@
 import EnterDOI from "app/core/components/EnterDOI"
+import Navbar from "app/core/components/Navbar"
 import { BlitzPage, useRouter, useSession } from "blitz"
 import { Suspense } from "react"
 
@@ -10,12 +11,12 @@ const Browse = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-darkest text-gray-darkest dark:text-white">
       <main className="flex-grow flex flex-col items-center">
-        <h1 id="post-review" className="mt-24 text-5xl font-bold">
+        <h1 id="post-review" className="mt-44 text-6xl font-bold text-gray-dark dark:text-white">
           PostReview
         </h1>
         <div
           id="search-bar-container"
-          className="absolute flex flex-grow mt-36 w-96 justify-center overflow-y-clip"
+          className="absolute flex flex-grow mt-60 mb-16 px-4 sm:px-2 md:px-80 w-full justify-center overflow-y-clip"
         >
           {session?.userId ? (
             <EnterDOI session={session} />
@@ -31,6 +32,7 @@ const Browse = () => {
 const BrowsePage: BlitzPage = () => {
   return (
     <Suspense fallback="Loading...">
+      <Navbar hideSearch={true} />
       <Browse />
     </Suspense>
   )
