f986cf04b9fafa21016c35f593767d6ef133bc74
jazellemaira <jazellemaira@gmail.com>
2022-08-24 17:04:34 -0400

    Create the frame for the team page

--- app/pages/team.tsx
+++ app/pages/team.tsx
@@ -2,3 +2,14 @@ import Navbar from "app/core/components/Navbar"
 import Layout from "app/core/layouts/Layout"
 import { BlitzPage, Image, Link } from "blitz"
 import { Suspense } from "react"
+const Team: BlitzPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      <Suspense fallback="Loading...">
+        <Navbar />
+      </Suspense>
+    </div>
+  )
+}
+Team.getLayout = (page) => <Layout title="Team | PostReview">{page}</Layout>
+export default Team
