00f1594c781fae0143d1385c2c8b2a9f2458297d
jazellemaira <jazellemaira@gmail.com>
2022-08-24 17:10:12 -0400

    Import photos for team page from public

--- app/pages/team.tsx
+++ app/pages/team.tsx
@@ -2,6 +2,11 @@ import Navbar from "app/core/components/Navbar"
 import Layout from "app/core/layouts/Layout"
 import { BlitzPage, Image, Link } from "blitz"
 import { Suspense } from "react"
+import teamMemberAntonPhoto from "public/team-member-anton-photo.png"
+import teamMemberNamiPhoto from "public/team-member-nami-photo.png"
+import teamMemberJazellePhoto from "public/team-member-jazelle-photo.png"
+import postReviewMentorPhoto from "public/team-mentor-chris-photo.png"
+
 const Team: BlitzPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
