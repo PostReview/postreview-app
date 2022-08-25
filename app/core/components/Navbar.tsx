df961be118fc3b7efcfdf0e91572553d7c30c229
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-24 21:27:10 -0400

    Bring out the menu icon into the navbar component

--- app/core/components/Navbar.tsx
+++ app/core/components/Navbar.tsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import { HeaderUserButton } from "./HeaderUserButton"
 import EnterDOI from "./EnterDOI"
 import { useRouter, useSession, useRouterQuery } from "blitz"
 import { DrawerMenu } from "./DrawerMenu"
+import { FaBars } from "react-icons/fa"
 
 export default function Navbar(props) {
   const { hideSearch } = props
@@ -11,10 +12,20 @@ export default function Navbar(props) {
   const isAtRoot = router.pathname === "/"
   const query = useRouterQuery()
 
+  const [open, setOpen] = useState(false)
+  const toggleDrawer = () => {
+    setOpen(!open)
+  }
+
   return (
     <>
       <nav className="bg-gradient-to-r from-gray-darkest to-gray-dark flex flex-row items-center h-16 fixed z-50 w-full">
-        <div className="w-16" />
+        <button
+          onClick={() => toggleDrawer()}
+          className={"mx-4 text-4xl top-4 left-4 text-gray-medium outline-white"}
+        >
+          <FaBars />
+        </button>
         <div id="search-bar-container" className="flex flex-grow justify-center">
           {hideSearch ? undefined : session?.userId ? (
             <EnterDOI session={session} />
@@ -25,7 +36,7 @@ export default function Navbar(props) {
         <div id="buttons-container" className="mx-4 flex flex-row">
           <HeaderUserButton />
         </div>
-        <DrawerMenu />
+        <DrawerMenu open={open} setOpen={setOpen} />
       </nav>
       <div className="h-16 w-full" />
     </>
