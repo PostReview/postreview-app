7eb36708ffc415041742b319ffca768a2c4d0b99
jazellemaira <jazellemaira@gmail.com>
2022-08-26 13:27:16 -0400

    Change header bg color ToU page

--- app/pages/terms-of-use.tsx
+++ app/pages/terms-of-use.tsx
@@ -96,8 +96,10 @@ const TermsofUsePage: BlitzPage = () => {
         <Navbar />
       </Suspense>
       <main className="flex-grow flex flex-col items-center bg-gray-light dark:bg-gray-darkest">
-        <div className="py-4 w-full text-3xl font-bold bg-gray-dark dark:bg-black/30">
-          <h1 className="text-center text-black dark:text-white">Terms of Use</h1>
+        <div className="py-4 w-full text-3xl font-bold bg-gray-medium dark:bg-black/60">
+          <h1 className="text-center bg-gray-medium dark:bg-black/0 text-black dark:text-white">
+            Terms of Use
+          </h1>
         </div>
         <div id="terms-of-use-image" className="py-4">
           <Image
