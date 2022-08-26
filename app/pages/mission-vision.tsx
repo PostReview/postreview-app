65ec8fccc1d8ac31e3d6730710e40d37b1afa788
jazellemaira <jazellemaira@gmail.com>
2022-08-26 17:13:42 -0400

    Import photos for mission vision page

--- app/pages/mission-vision.tsx
+++ app/pages/mission-vision.tsx
@@ -2,3 +2,7 @@ import Navbar from "app/core/components/Navbar"
 import Layout from "app/core/layouts/Layout"
 import { BlitzPage, Image, Link } from "blitz"
 import { Suspense } from "react"
+import transparencyPhoto from "public/transparency-photo.png"
+import diversityPhoto from "public/diversity-photo.png"
+import accountabilityPhoto from "public/accountability-photo.png"
+
