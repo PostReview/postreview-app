e363c79fd02b408e0eae2421f5351ab7661ffe09
jazellemaira <jazellemaira@gmail.com>
2022-08-26 17:15:16 -0400

    Make mission vision page

--- app/pages/mission-vision.tsx
+++ app/pages/mission-vision.tsx
@@ -6,3 +6,11 @@ import transparencyPhoto from "public/transparency-photo.png"
 import diversityPhoto from "public/diversity-photo.png"
 import accountabilityPhoto from "public/accountability-photo.png"
 
+const MissionVisionPage: BlitzPage = () => {
+  return (
+  )
+}
+MissionVisionPage.getLayout = (page) => (
+  <Layout title="Mission and Vision | PostReview">{page}</Layout>
+)
+export default MissionVisionPage
