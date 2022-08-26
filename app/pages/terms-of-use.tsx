9ea9f95de986007621fe745387304fd46834261a
jazellemaira <jazellemaira@gmail.com>
2022-08-26 13:29:01 -0400

    Change font weight & bg color expand button ToU

--- app/pages/terms-of-use.tsx
+++ app/pages/terms-of-use.tsx
@@ -112,7 +112,7 @@ const TermsofUsePage: BlitzPage = () => {
         <div id="terms-of-use-content" className="max-w-3xl">
           <div className="text-right">
             <button
-              className="px-2 py-1 mx-3 my-2 text-sm bg-gray-medium text-black dark:text-gray-light"
+              className="px-2 py-1 mx-3 my-2 text-sm font-semibold bg-gray-dark/40 dark:bg-black/40 text-black dark:text-gray-light"
               onClick={() => handleExpandAll(!expandClicked)}
             >
               {expandClicked ? "Collapse" : "Expand All"}
