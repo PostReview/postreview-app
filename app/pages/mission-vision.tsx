5bf42a3e84c74714331291fee35f435bb43d8769
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-27 17:16:23 -0400

    Remove suspense from the mission page

--- app/pages/mission-vision.tsx
+++ app/pages/mission-vision.tsx
@@ -1,7 +1,6 @@
 import Navbar from "app/core/components/Navbar"
 import Layout from "app/core/layouts/Layout"
 import { BlitzPage, Image } from "blitz"
-import { Suspense } from "react"
 import transparencyPhoto from "public/transparency-photo.png"
 import diversityPhoto from "public/diversity-photo.png"
 import accountabilityPhoto from "public/accountability-photo.png"
@@ -9,9 +8,7 @@ import accountabilityPhoto from "public/accountability-photo.png"
 const MissionVisionPage: BlitzPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
-      <Suspense fallback="Loading...">
-        <Navbar />
-      </Suspense>
+      <Navbar />
       <main className="mb-20 flex-grow flex flex-col items-center text-gray-darkest">
         <h1 className="mt-12 text-5xl font-bold">Our Mission</h1>
         <p className="mt-4 mx-4 text-center text-xl max-w-xl">
