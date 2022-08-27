8c42e7d26276556df189fb2a011bc5a8fe7a4be0
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-27 16:58:40 -0400

    Remove suspense and specify layout from how-it-works pg

--- app/pages/how-it-works.tsx
+++ app/pages/how-it-works.tsx
@@ -2,18 +2,19 @@ import Navbar from "app/core/components/Navbar"
 import { HowItWorks } from "app/core/components/HowItWorks"
 import { BlitzPage } from "blitz"
 import { Suspense } from "react"
+import Layout from "app/core/layouts/Layout"
 
 const HowItWorksPage: BlitzPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Suspense fallback="Loading...">
-        <Navbar />
-      </Suspense>
+      <Navbar />
       <main className="flex-grow flex flex-col items-center bg-white dark:bg-gray-darkest">
         <HowItWorks />
       </main>
     </div>
   )
 }
+
+HowItWorksPage.getLayout = (page) => <Layout title="How it works | PostReview">{page}</Layout>
 
 export default HowItWorksPage
