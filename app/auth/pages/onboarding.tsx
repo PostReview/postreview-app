d69d67e3acbdcb54a51ecc74f0b5db054c9c087d
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-28 17:28:23 -0400

    Factor out the UploadYourPhoto component

--- app/auth/pages/onboarding.tsx
+++ app/auth/pages/onboarding.tsx
@@ -1,15 +1,11 @@
-import { useRouter, BlitzPage, Image, useSession } from "blitz"
+import { useRouter, BlitzPage, useSession } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "app/core/components/Navbar"
-import { Button } from "app/core/components/Button"
-import detectiveDarkMode from "public/detective-darkmode.png"
-import detectiveLightMode from "public/detective-lightmode.png"
-import { AvatarIcon } from "app/core/components/AvatarIcon"
-import { Widget } from "@uploadcare/react-widget"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import { GetStarted } from "app/core/components/onboarding/GetStarted"
 import { EnterName } from "app/core/components/onboarding/EnterName"
+import { UploadYourPhoto } from "app/core/components/onboarding/UploadYourPhoto"
 
 const OnboardingPage: BlitzPage = () => {
   // Redirect when not logged in
@@ -32,78 +28,8 @@ const OnboardingPage: BlitzPage = () => {
   const defaultCurrentUser = useCurrentUser()
   const [currentUser, setCurrentUser] = useState(defaultCurrentUser)
 
-  // Uploadcare
-  const UPLOADCARE_PUBLIC_KEY = process.env.UPLOADCARE_PUBLIC_KEY
-  const widgetApi = useRef<any>()
-
   // Handle pages
   const [currentPage, setCurrentPage] = useState("get-started")
-
-  const UploadYourPhoto = () => {
-    return (
-      <div id="upload-photo-container" className="text-white">
-        <div id="title-container" className="flex flex-row items-center ">
-          <div className="text-gray-darkest dark:text-white">
-            <h1 className="text-4xl font-bold my-4">Upload your photo</h1>
-            <div className="mx-auto w-fit">But, only if you want to!</div>
-          </div>
-          <div className="ml-4 contrast-100 m-6 w-24">
-            <Image
-              src={isDark ? detectiveDarkMode : detectiveLightMode}
-              alt="An image of a detective looking through a magnifying glass with their left eye"
-              width={584}
-              height={800}
-            />
-          </div>
-        </div>
-        <div id="profile-picture" className="flex flex-row mx-4">
-          <Button
-            id="user-avatar"
-            className="focus:outline-none"
-            onClick={() => widgetApi?.current?.openDialog()}
-          >
-            <AvatarIcon currentUser={currentUser} height={"5rem"} width={"5rem"} />
-          </Button>
-          <div className="hidden">
-            <Widget
-              publicKey={UPLOADCARE_PUBLIC_KEY ? UPLOADCARE_PUBLIC_KEY : ""}
-              ref={widgetApi}
-              crop="1:1"
-              imageShrink="480x480"
-              imagesOnly
-              previewStep
-              clearable
-              tabs="file url"
-              onChange={async (info) => {
-                try {
-                  // setUserInfo({ id: userInfo.id, icon: icon, ...userInfo })
-                  // setIcon(info.cdnUrl)
-                } catch (err) {
-                  alert(err)
-                }
-              }}
-            />
-          </div>
-          <div className="self-end mx-3 text-white">
-            {currentUser?.pronoun && `(${currentUser.pronoun})`}
-          </div>
-        </div>
-        <div className="ml-4 mt-2 text-2xl font-semibold">{currentUser?.displayName}</div>
-        <div className="ml-4 text-xl">{`@${currentUser?.handle}`}</div>
-        <div id="buttons-container" className="flex flex-col items-center text-2xl pb-28 mt-10">
-          <button
-            className="w-min whitespace-nowrap my-4 px-8 py-2 text-green rounded-lg bg-black/50 hover:bg-gray-darkest dark:bg-gray-medium dark:hover:bg-black/40"
-            onClick={() => router.push("/")}
-          >
-            Save
-          </button>
-          <button className="w-min my-4 text-gray-medium" onClick={() => router.push("/")}>
-            Skip
-          </button>
-        </div>
-      </div>
-    )
-  }
 
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-darkest">
@@ -120,7 +46,14 @@ const OnboardingPage: BlitzPage = () => {
             setCurrentUser={setCurrentUser}
           />
         )}
-        {currentPage === "upload-photo" && <UploadYourPhoto />}
+        {currentPage === "upload-photo" && (
+          <UploadYourPhoto
+            isDark={isDark}
+            router={router}
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />
+        )}
       </main>
     </div>
   )
---
+++ app/core/components/onboarding/UploadYourPhoto.tsx
@@ -0,0 +1,79 @@
+import { Widget } from "@uploadcare/react-widget"
+import { Image } from "blitz"
+import React, { useRef } from "react"
+import { AvatarIcon } from "../AvatarIcon"
+import { Button } from "../Button"
+import detectiveDarkMode from "public/detective-darkmode.png"
+import detectiveLightMode from "public/detective-lightmode.png"
+
+export const UploadYourPhoto = (props) => {
+  const { isDark, currentUser, setCurrentUser, router } = props
+
+  // Uploadcare
+  const UPLOADCARE_PUBLIC_KEY = process.env.UPLOADCARE_PUBLIC_KEY
+  const widgetApi = useRef<any>()
+
+  return (
+    <div id="upload-photo-container" className="text-white">
+      <div id="title-container" className="flex flex-row items-center ">
+        <div className="text-gray-darkest dark:text-white">
+          <h1 className="text-4xl font-bold my-4">Upload your photo</h1>
+          <div className="mx-auto w-fit">But, only if you want to!</div>
+        </div>
+        <div className="ml-4 contrast-100 m-6 w-24">
+          <Image
+            src={isDark ? detectiveDarkMode : detectiveLightMode}
+            alt="An image of a detective looking through a magnifying glass with their left eye"
+            width={584}
+            height={800}
+          />
+        </div>
+      </div>
+      <div id="profile-picture" className="flex flex-row mx-4">
+        <Button
+          id="user-avatar"
+          className="focus:outline-none"
+          onClick={() => widgetApi?.current?.openDialog()}
+        >
+          <AvatarIcon currentUser={currentUser} height={"5rem"} width={"5rem"} />
+        </Button>
+        <div className="hidden">
+          <Widget
+            publicKey={UPLOADCARE_PUBLIC_KEY ? UPLOADCARE_PUBLIC_KEY : ""}
+            ref={widgetApi}
+            crop="1:1"
+            imageShrink="480x480"
+            imagesOnly
+            previewStep
+            clearable
+            tabs="file url"
+            onChange={async (info) => {
+              try {
+                // setUserInfo({ id: userInfo.id, icon: icon, ...userInfo })
+                // setIcon(info.cdnUrl)
+              } catch (err) {
+                alert(err)
+              }
+            }}
+          />
+        </div>
+        <div className="self-end mx-3 text-white">
+          {currentUser?.pronoun && `(${currentUser.pronoun})`}
+        </div>
+      </div>
+      <div className="ml-4 mt-2 text-2xl font-semibold">{currentUser?.displayName}</div>
+      <div className="ml-4 text-xl">{`@${currentUser?.handle}`}</div>
+      <div id="buttons-container" className="flex flex-col items-center text-2xl pb-28 mt-10">
+        <button
+          className="w-min whitespace-nowrap my-4 px-8 py-2 text-green rounded-lg bg-black/50 hover:bg-gray-darkest dark:bg-gray-medium dark:hover:bg-black/40"
+          onClick={() => router.push("/")}
+        >
+          Save
+        </button>
+        <button className="w-min my-4 text-gray-medium" onClick={() => router.push("/")}>
+          Skip
+        </button>
+      </div>
+    </div>
+  )
+}
