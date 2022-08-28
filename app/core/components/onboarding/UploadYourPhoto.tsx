b56517df4965c9bf57fe41dd3c6055e6e2102554
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-28 18:58:45 -0400

    Update user avatar on onboarding

--- app/core/components/onboarding/UploadYourPhoto.tsx
+++ app/core/components/onboarding/UploadYourPhoto.tsx
@@ -1,6 +1,6 @@
 import { Widget } from "@uploadcare/react-widget"
 import { Image } from "blitz"
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import { AvatarIcon } from "../AvatarIcon"
 import { Button } from "../Button"
 import detectiveDarkMode from "public/detective-darkmode.png"
@@ -13,11 +13,20 @@ export const UploadYourPhoto = (props) => {
   const UPLOADCARE_PUBLIC_KEY = process.env.UPLOADCARE_PUBLIC_KEY
   const widgetApi = useRef<any>()
 
+  // Track avatar icon
+  const [icon, setIcon] = useState(currentUser.icon)
+
+  // Handle the save button
+  const handleSave = () => {
+    changeUserInfoMutation({ ...currentUser, icon: icon })
+    router.push("/")
+  }
+
   return (
     <div id="upload-photo-container" className="text-white">
       <div id="title-container" className="flex flex-row items-center ">
         <div className="text-gray-darkest dark:text-white">
-          <h1 className="text-4xl font-bold my-4">Upload your photo</h1>
+          <h1 className="text-4xl font-bold mx-6 my-4">Upload your photo</h1>
           <div className="mx-auto w-fit">But, only if you want to!</div>
         </div>
         <div className="ml-4 contrast-100 m-6 w-24">
@@ -29,13 +38,17 @@ export const UploadYourPhoto = (props) => {
           />
         </div>
       </div>
-      <div id="profile-picture" className="flex flex-row mx-4">
+      <div id="profile-picture" className="flex flex-row justify-center mx-4 my-8">
         <Button
           id="user-avatar"
           className="focus:outline-none"
           onClick={() => widgetApi?.current?.openDialog()}
         >
-          <AvatarIcon currentUser={currentUser} height={"5rem"} width={"5rem"} />
+          <AvatarIcon
+            currentUser={{ ...currentUser, icon: icon }}
+            height={"10rem"}
+            width={"10rem"}
+          />
         </Button>
         <div className="hidden">
           <Widget
@@ -49,24 +62,24 @@ export const UploadYourPhoto = (props) => {
             tabs="file url"
             onChange={async (info) => {
               try {
-                // setUserInfo({ id: userInfo.id, icon: icon, ...userInfo })
-                // setIcon(info.cdnUrl)
+                setCurrentUser({ ...currentUser, icon: icon })
+                setIcon(info.cdnUrl)
               } catch (err) {
                 alert(err)
               }
             }}
           />
         </div>
-        <div className="self-end mx-3 text-white">
+        {/* <div className="self-end mx-3 text-white">
           {currentUser?.pronoun && `(${currentUser.pronoun})`}
-        </div>
+        </div> */}
       </div>
-      <div className="ml-4 mt-2 text-2xl font-semibold">{currentUser?.displayName}</div>
-      <div className="ml-4 text-xl">{`@${currentUser?.handle}`}</div>
+      {/* <div className="ml-4 mt-2 text-2xl font-semibold">{currentUser?.displayName}</div>
+      <div className="ml-4 text-xl">{`@${currentUser?.handle}`}</div> */}
       <div id="buttons-container" className="flex flex-col items-center text-2xl pb-28 mt-10">
         <button
           className="w-min whitespace-nowrap my-4 px-8 py-2 text-green rounded-lg bg-black/50 hover:bg-gray-darkest dark:bg-gray-medium dark:hover:bg-black/40"
-          onClick={() => router.push("/")}
+          onClick={() => handleSave()}
         >
           Save
         </button>
