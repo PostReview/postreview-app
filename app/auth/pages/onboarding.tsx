bfcd2f3202e4cf0e224b58fbca3115e545ec826e
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-28 14:45:19 -0400

    Create EnterName component

--- app/auth/pages/onboarding.tsx
+++ app/auth/pages/onboarding.tsx
@@ -2,8 +2,7 @@ import { useRouter, BlitzPage, invoke, useMutation, Image, Link, Routes } from "
 import Layout from "app/core/layouts/Layout"
 import { Suspense, useEffect, useState } from "react"
 import Navbar from "app/core/components/Navbar"
-import { Formik } from "formik"
-import GoogleButton from "app/core/components/GoogleButton"
+import { ErrorMessage, Field, Form, Formik } from "formik"
 import getUserInfo from "app/queries/getUserInfo"
 import signup from "../mutations/signup"
 import { Button } from "app/core/components/Button"
@@ -70,11 +69,69 @@ const OnboardingPage: BlitzPage = () => {
     )
   }
 
+  const EnterName = () => {
+    return (
+      <div id="enter-name-container" className="pb-20">
+        <div id="title-container" className="flex flex-row items-center">
+          <div className="ml-4 contrast-100 m-6 w-24">
+            <Image
+              src={isDark ? detectiveDarkMode : detectiveLightMode}
+              alt="An image of a detective looking through a magnifying glass with their left eye"
+              width={584}
+              height={800}
+            />
+          </div>
+          <h1 className="w-64 text-4xl font-bold my-4 text-gray-darkest dark:text-white">
+            How do you want to be addressed?
+          </h1>
+        </div>
+        <Formik
+          initialValues={{
+            displayName: "",
+            pronoun: "",
+          }}
+          onSubmit={async (values) => {
+            // setUserInfo({ id: userInfo.id, icon: icon, ...values })
+            // changeUserinfoMutation({ id: userInfo.id, ...values, icon: icon })
+            // setOpen(false)
+            // Reload the page to reflect the change on the user avatar in the nav bar
+            // (TODO: Make the icon change reactive in the nav bar)
+            window.location.reload()
+          }}
+          validate={(values) => {}}
+        >
+          <Form className="flex flex-col text-xl bg-black text-white px-4">
+            <div className="flex flex-row border-b border-gray-dark py-4">
+              <label className="w-32">Name</label>
+              <Field
+                name="displayName"
+                type="text"
+                className="ml-1 bg-black outline-0 text-green"
+              />
+            </div>
+            <div className="flex flex-row py-4">
+              <label htmlFor="pronouns" className="w-32">
+                Pronouns
+              </label>
+              <Field name="pronoun" type="text" className="ml-1 bg-black outline-0 text-green" />
+            </div>
+          </Form>
+        </Formik>
+        <div className="text-center">
+          <button id="next-button" className="m-9 text-green text-2xl">
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-darkest">
       <Navbar hideSearch={true} />
       <main className="flex-grow flex flex-col items-center mt-36">
         <GetStarted />
+        <EnterName />
       </main>
     </div>
   )
