dbdb5b0a55d57550d223260a8ac4a970ed94d5de
jazellemaira <jazellemaira@gmail.com>
2022-08-28 17:37:16 -0400

    Add main badge container

--- app/core/components/ThankYouBadge.tsx
+++ app/core/components/ThankYouBadge.tsx
@@ -3,6 +3,10 @@ import { Image } from "blitz"
 
 const ThankYouBadge = (props) => {
   return (
+    <>
+      <div id="badge-container" className="flex flex-col relative min-h-screen items-center mt-56">
+      </div>
+    </>
   )
 }
 
