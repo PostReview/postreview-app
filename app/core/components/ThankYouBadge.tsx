d398b00cb3d335f63300a9f227aae5090aebd54c
jazellemaira <jazellemaira@gmail.com>
2022-08-28 17:38:33 -0400

    Add the thank you badge photo

--- app/core/components/ThankYouBadge.tsx
+++ app/core/components/ThankYouBadge.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
+import thankYouBadge from "public/thank-you-badge.png"
 import { Image } from "blitz"
 
 const ThankYouBadge = (props) => {
   return (
     <>
       <div id="badge-container" className="flex flex-col relative min-h-screen items-center mt-56">
+        <Image src={thankYouBadge} alt="An image of a badge container" width={400} height={400} />
       </div>
     </>
   )
