7183cfeeb431717b81f952457143c38c08bf2f75
jazellemaira <jazellemaira@gmail.com>
2022-08-28 17:39:09 -0400

    Add constant thank you message

--- app/core/components/ThankYouBadge.tsx
+++ app/core/components/ThankYouBadge.tsx
@@ -7,6 +7,12 @@ const ThankYouBadge = (props) => {
     <>
       <div id="badge-container" className="flex flex-col relative min-h-screen items-center mt-56">
         <Image src={thankYouBadge} alt="An image of a badge container" width={400} height={400} />
+        <div
+          id="thank-you-message"
+          className="absolute text-md text-center top-56 pt-1 font-semibold text-gray-dark"
+        >
+          Thanks for reviewing!
+        </div>
       </div>
     </>
   )
