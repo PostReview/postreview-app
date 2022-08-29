9884c307bcbe6644ae2f5aad640175352025dfba
jazellemaira <jazellemaira@gmail.com>
2022-08-29 13:41:48 -0400

    Make  drawer bg gray light

--- app/core/components/DrawerMenu.tsx
+++ app/core/components/DrawerMenu.tsx
@@ -48,6 +48,13 @@ export const DrawerMenu = (props) => {
   return (
     <>
       <Drawer
+        anchor={"left"}
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{
+          background: "#d9d9d9",
+        }}
+      >
         <div className="w-full h-full bg-gray-light flex flex-col overflow-clip">
           <div id="logo-container" className="ml-8">
             <Image src={postReviewIcon} alt={"A magnifier with a hat"} height={250} width={250} />
