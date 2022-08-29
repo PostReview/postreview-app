383a736198a6da681add776255b52c3923a6a64b
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-29 18:16:29 -0400

    Use title or name props for search results

--- app/core/components/SearchResultArticle.tsx
+++ app/core/components/SearchResultArticle.tsx
@@ -2,7 +2,7 @@ const SearchResultArticle = ({ item, components }) => {
   return (
     <>
       <div className="my-1 mx-1 flex-col">
-        <div className="mr-2">{item.title}</div>
+        <div className="mr-2">{item.title || item.name}</div>
         <div>
           <p className="text-xs text-gray-500 dark:text-gray-400">
             {item?.authors} {item.publishedYear && `(${item.publishedYear})`}
