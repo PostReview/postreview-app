d11c5008bff6ba498febd822543386a5b83fe1e2
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-30 16:54:21 -0400

    Update the users index (not articles)

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -8,7 +8,7 @@ import algoliasearch from "algoliasearch"
 
 // Initialize Algolia
 const client = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_API_ADMIN_KEY!)
-const index = client.initIndex(`${process.env.ALGOLIA_PREFIX}_articles`)
+const index = client.initIndex(`${process.env.ALGOLIA_PREFIX}_users`)
 
 export default resolver.pipe(resolver.zod(Signup), async ({ email, handle, password }, ctx) => {
   const hashedPassword = await SecurePassword.hash(password.trim())
