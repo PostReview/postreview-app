46c9cdd6924661a009d2167e005300b3c1d55ac6
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-30 17:27:14 -0400

    Delete Algolia record when deleting user

--- app/mutations/deleteUser.tsx
+++ app/mutations/deleteUser.tsx
@@ -1,5 +1,10 @@
 import db from "db"
 import { Ctx, AuthorizationError } from "blitz"
+import algoliasearch from "algoliasearch"
+
+// Initialize Algolia
+const client = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_API_ADMIN_KEY!)
+const index = client.initIndex(`${process.env.ALGOLIA_PREFIX}_users`)
 
 export default async function deleteUser(currentUserId, ctx: Ctx) {
   ctx.session.$authorize()
@@ -9,6 +14,9 @@ export default async function deleteUser(currentUserId, ctx: Ctx) {
   }
 
   const result = await db.user.delete({ where: { id: currentUserId } })
+
+  // Update Algolia
+  await index.deleteObject(currentUserId)
 
   return result
 }
