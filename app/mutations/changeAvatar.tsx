1ad2fc9bd90f14f396d2d9cf3558533add74170c
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-30 16:55:08 -0400

    Update Algolia when updating Avatar

--- app/mutations/changeAvatar.tsx
+++ app/mutations/changeAvatar.tsx
@@ -1,6 +1,11 @@
 import db from "db"
 import { Ctx, AuthorizationError } from "blitz"
 import * as z from "zod"
+import algoliasearch from "algoliasearch"
+
+// Initialize Algolia
+const client = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_API_ADMIN_KEY!)
+const index = client.initIndex(`${process.env.ALGOLIA_PREFIX}_users`)
 
 const inputData = z.object({
   id: z.number(),
@@ -21,5 +26,12 @@ export default async function changeAvatar(input: z.infer<typeof inputData>, ctx
       icon: data.avatar,
     },
   })
+
+  // Update algolia
+  await index.partialUpdateObject({
+    objectID: data.id,
+    icon: data.avatar,
+  })
+
   return result
 }
