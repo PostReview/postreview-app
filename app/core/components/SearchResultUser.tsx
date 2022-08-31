5b1bc6339586350be385c42e253d1eb316702106
Nami Sunami <naoyuki.sunami@gmail.com>
2022-08-31 18:08:32 -0400

    Use `AvatarIcon` component in search results

--- app/core/components/SearchResultUser.tsx
+++ app/core/components/SearchResultUser.tsx
@@ -1,11 +1,16 @@
 import { Image } from "blitz"
+import { AvatarIcon } from "./AvatarIcon"
 
 const SearchResultUser = ({ item, components }) => {
   return (
     <>
       <div className="my-1 mx-1 flex flex-row">
         <div id="avatar">
-          <Image src={item.icon} alt="avatar icon" width={30} height={30} />
+          <AvatarIcon
+            currentUser={{ handle: item?.handle, icon: item?.icon, displayName: item?.displayName }}
+            width={30}
+            height={30}
+          />
         </div>
         <div id="user-metadata" className="flex flex-col ml-3">
           <div>{item?.displayName}</div>
